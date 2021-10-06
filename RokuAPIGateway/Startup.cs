3193b22921f723f6302b793dea2d5d688477e5e8
nick van Oijen <I468166@fhict.nl>
2021-10-06 17:01:28 +0200

    Tweak Fixed middleware, needs to ad header to downstream request

--- RokuAPIGateway/Startup.cs
+++ RokuAPIGateway/Startup.cs
@@ -19,6 +19,10 @@ using Ocelot.Errors;
 using Ocelot.LoadBalancer.Middleware;
 using Ocelot.DownstreamUrlCreator.Middleware;
 using Ocelot.Requester.Middleware;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace RokuAPIGateway
 {
@@ -41,30 +45,73 @@ namespace RokuAPIGateway
 
             app.UseRouting();
 
-            app.UseOcelot((ocelotBuilder, ocelotConfiguration) =>
-            {
-                //app.UseDownstreamContextMiddleware();
-                
-                ocelotBuilder.UseResponderMiddleware();
-
-                ocelotBuilder.UseMiddleware<JWTMiddleware>();
-
-
-                //ocelotBuilder.UseDownstreamRouteFinderMiddleware();
-                //ocelotBuilder.UseMultiplexingMiddleware();
-                //ocelotBuilder.UseDownstreamRequestInitialiser();
-                //ocelotBuilder.UseRequestIdMiddleware();
- 
-                ocelotBuilder.Use((ctx, next) =>
+            //If JWT TOKen valid response fails
+            //If jWT Token not valid response no succes
+            //If no JWT Token Go to downstream 
+            var config = new OcelotPipelineConfiguration {
+                AuthenticationMiddleware = async (ctx, next) =>
                 {
-                    //ctx.Items.SetError(new UnauthorizedError("unauthorized", OcelotErrorCode.UnauthorizedError, 400));
-                    return Task.CompletedTask;
-                });
+                    if (!ctx.Request.Headers.ContainsKey("X-JWT-Token"))
+                    {
+                        await next.Invoke();
+                        return;
+                    }
 
-                ocelotBuilder.UseLoadBalancingMiddleware();
-                //ocelotBuilder.UseDownstreamUrlCreatorMiddleware();
-                //ocelotBuilder.UseHttpRequesterMiddleware();
-            }).Wait();
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri("http://localhost:7000/");
+                    client.DefaultRequestHeaders.Add("X-JWT-Token", (string)ctx.Request.Headers["X-JWT-Token"]);
+
+                    HttpResponseMessage responseMessage = await client.GetAsync("25a57dae7cca4fcfb52327966462310e/user/verify");
+                    if(responseMessage == null)
+                    {                       
+                        ctx.Items.SetError(new UnauthorizedError("Response null", OcelotErrorCode.UnauthenticatedError, 401));
+                        return;
+                    }
+                    
+                    if (!responseMessage.IsSuccessStatusCode)
+                    {
+                        ctx.Items.SetError(new UnauthorizedError($"Response failed: {responseMessage.StatusCode}", OcelotErrorCode.UnauthenticatedError, 401));
+
+                        return;
+                    }
+
+
+                    Message message = new Message();
+
+                    var contentStream = await responseMessage.Content.ReadAsStreamAsync();
+                    using var streamReader = new StreamReader(contentStream);
+                    using var jsonReader = new JsonTextReader(streamReader);
+
+                    JsonSerializer serializer = new JsonSerializer();
+
+                    try
+                    {
+                        message = serializer.Deserialize<Message>(jsonReader);
+                    }
+                    catch (JsonReaderException)
+                    {
+                        Console.WriteLine("Invalid JSON.");
+                    }
+                    
+                    if (!message.Succes)
+                    {
+                      
+                        ctx.Items.SetError(new UnauthorizedError("Response false", OcelotErrorCode.UnauthenticatedError, 200));
+                        
+                        return;
+                    }
+                    //(string)message.Object.UserId
+                    ctx.Request.Headers.Add("X-User-Validated", "gaming");
+                    ctx.Response.Headers.Add("X-User-Validated", "haming");
+                    
+                    await next.Invoke();
+                }
+            };
+
+            app.UseOcelot(config).Wait();
+
         }
     }
+
+
 }
